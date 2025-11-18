0947d0bb8a9934623859336e5c07f988ce19b00d
zedfar <ulfar.far@gmail.com>
2025-11-18 19:35:26 +0700

    update

--- local-news-app/node_modules/expo-task-manager/android/src/main/java/expo/modules/taskManager/EmitEventWrapper.java
+++
@@ -1,9 +0,0 @@
-package expo.modules.taskManager;
-
-import android.os.Bundle;
-
-// Wrapper used for passing the emit function from the EventEmitter between Kotlin and Java
-@FunctionalInterface
-interface EmitEventWrapper {
-  void emit(String name, Bundle body);
-}
